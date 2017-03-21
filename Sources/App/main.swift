d70aa693fdf96dd1c4ea221d311858255ca17bf4
Leo Moon <leomoondev@gmail.com>
2017-03-20 18:49:14 -0700

    Add a string as a parameter and respond with a uppercased string

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,10 +2,22 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get("hello") { request in
+    return "Hello, world!"
+}
+
+drop.get("upcase") { request in
+    var str = "EMPTY"
+    return str
+    
+}
+
+drop.get("upcase") { request in
+    guard let rating = request.query?["words"]?.string else {
+        throw Abort.custom(status: .preconditionFailed, message: "Please include a word")
+    }
+    
+    return "You requested comments with rating greater than \(rating.uppercased())"
 }
 
 drop.resource("posts", PostController())
